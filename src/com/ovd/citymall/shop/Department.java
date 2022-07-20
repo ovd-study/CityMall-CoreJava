8be1e7d348ea8ba0bbb0cf53eca71f24d2c6b204
Ojas Deshmukh <ovd.study@gmail.com>
2022-07-20 13:52:11 -0400

    Fixed the error of items not displaying

--- src/com/ovd/citymall/shop/Department.java
+++ src/com/ovd/citymall/shop/Department.java
@@ -35,60 +35,46 @@ public class Department {
 			itemList.add(new Item("Pant", 25.25));
 		}
 		// Sporting
-		else if (name.equalsIgnoreCase("Men")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Women")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
+		else if (name.equalsIgnoreCase("Basketball")) {
+			itemList.add(new Item("Ball", 79.99));
+			itemList.add(new Item("Hoop", 500.00));
+			itemList.add(new Item("Shoes", 200.00));
+		} else if (name.equalsIgnoreCase("Soccer")) {
+			itemList.add(new Item("Jersey", 65.00));
+			itemList.add(new Item("Ball", 25.00));
+			itemList.add(new Item("Cleats", 75.00));
+		} else if (name.equalsIgnoreCase("Football")) {
+			itemList.add(new Item("Ball", 35.25));
+			itemList.add(new Item("Helmet", 55.50));
+			itemList.add(new Item("Sholder Pads", 65.00));
 		}
 		// Furniture
-		else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Women")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
+		else if (name.equalsIgnoreCase("Kitchen")) {
+			itemList.add(new Item("Silverware", 36.50));
+			itemList.add(new Item("Refrigerator", 2000.00));
+			itemList.add(new Item("Airfryer", 300.00));
+		} else if (name.equalsIgnoreCase("Living")) {
+			itemList.add(new Item("Couch", 1500.00));
+			itemList.add(new Item("Media Stand", 161.25));
+			itemList.add(new Item("Rug", 400.00));
+		} else if (name.equalsIgnoreCase("Bed")) {
+			itemList.add(new Item("Bed Frame", 500.00));
+			itemList.add(new Item("Dresser", 200.00));
+			itemList.add(new Item("Nigh Stand", 25.25));
 		}
 		// Book
-		else if (name.equalsIgnoreCase("Women")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		}
-
-		else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
-		} else if (name.equalsIgnoreCase("Kid")) {
-			itemList.add(new Item("Shirt", 15.25));
-			itemList.add(new Item("Short", 10.25));
-			itemList.add(new Item("Pant", 25.25));
+		else if (name.equalsIgnoreCase("Miscellaneous")) {
+			itemList.add(new Item("Sherlock Holmes", 15.25));
+			itemList.add(new Item("The Great Gatsby", 10.25));
+			itemList.add(new Item("No Longer Human", 25.25));
+		} else if (name.equalsIgnoreCase("Graphic Novels")) {
+			itemList.add(new Item("Marvel Series", 15.25));
+			itemList.add(new Item("DC Comics", 10.25));
+			itemList.add(new Item("Friends Game", 25.25));
+		} else if (name.equalsIgnoreCase("Light Novels")) {
+			itemList.add(new Item("Classroom of the elite", 15.25));
+			itemList.add(new Item("The Beginning after the end", 10.25));
+			itemList.add(new Item("Solo Leveling", 25.25));
 		}
 	}
 
